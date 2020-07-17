1e1aa07243bd76781bee41f76a0f2cefdb6fed3d
Hiroto Nishidate <tak713tak@gmail.com>
2020-07-17 07:44:56 +0000

    commit

---
+++ resources/views/layouts/hooder.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-info">
+  <a href="#" class="navbar-brand">Find a Western restaurantとは</a>
+  <div class="collapse navbar-collapse justify-content-end" id="navmenu">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">お問い合わせ</a>
+      <a class="nav-item nav-link active" href="#">利用規約</a>
+    </div>
+  </div>
+</nav>
